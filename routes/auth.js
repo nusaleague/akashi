0d555294a2336f94a52dca5f013be3f56dc2e761
Ted Kesgar <t.kesgar@live.com>
2019-04-27 14:11:39 +0700

    Tambah auth strategy staff

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const {Router: router} = require('express')
+const {Router: router, urlencoded} = require('express')
 const passport = require('../lib/passport')
 
 const route = router()
@@ -11,6 +11,30 @@ route.get('/auth/logout', (req, res) => {
   req.logout()
   res.sendStatus(204)
 })
+
+route.post('/auth/staff',
+  urlencoded({extended: false}),
+  (req, res, next) => passport.authenticate('staff', (err, user) => {
+    if (err) {
+      next(err)
+      return
+    }
+
+    if (!user) {
+      res.sendStatus(401)
+      return
+    }
+
+    req.login(user, err => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.sendStatus(200)
+    })
+  })(req, res, next)
+)
 
 for (const provider of ['facebook', 'twitter', 'google']) {
   route.get(`/auth/${provider}`,
